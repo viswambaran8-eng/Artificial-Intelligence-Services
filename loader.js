bb6d5069f55f130bf793204ea489af073ee000e0
sreejith <viswambaran8@gmail.com>
2025-12-23 12:47:09 +0530

    12345

---
+++ loader.js
@@ -0,0 +1,24 @@
+const circle = document.querySelector('.circle');
+const dots = circle.getAttribute('dots');
+
+let points = "";
+const rotate = 360 / dots;
+
+/* set rotation variable once */
+circle.style.setProperty('--rot', `${rotate}deg`);
+
+for (let i = 0; i < dots; i++) {
+  points += `<div class="point" style="--i:${i}"></div>`;
+}
+
+circle.innerHTML = points;
+
+// Load Out
+
+const LoaderApp = document.querySelector('.body1');
+const MainContent = document.querySelector('.the-main');
+
+setTimeout(() => {
+  LoaderApp.style.display = 'none';
+  MainContent.style.opacity = '1';
+}, 4000); // 4 seconds
